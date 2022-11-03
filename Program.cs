0217f975ba549308ef18a901377dfbd68793c4d2
Vlad <vladislav.klimenok@gmail.com>
2022-11-03 19:23:55 +0300

    2. Метод заполнения массива случайными символами

--- Program.cs
+++ Program.cs
@@ -12,3 +12,18 @@ string[] CreateArrayStrings(int size)
 {
     return new string[size];
 }
+
+// 2. Метод заполнения массива случайными символами
+void FillArrayStrings(string[] text, string selectChar, int lengthItemMin, int lengthItemMax)
+{
+    int size = text.Length;
+    int count = selectChar.Length;
+    for (int i = 0; i < size; i++)
+    {
+        int lengthItem = new Random().Next(lengthItemMin, lengthItemMax + 1);
+        for (int j = 0; j < lengthItem; j++)
+        {
+            text[i] += selectChar[new Random().Next(0, count)];
+        }
+    }
+}
